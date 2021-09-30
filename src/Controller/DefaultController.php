64c55b15bf69c7a074d6ba3f9948565a0f60849e
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-09-30 20:26:14 +0200

    Add translations to polish

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,13 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
+    {
+        return $this->render('pages/homepage.html.twig', [
+        ]);
+    }
+    #[Route(['en'=>'/about-me','pl'=>'/o-mnie'], name: 'app_about')]
+    public function test(): Response
     {
         return $this->render('pages/homepage.html.twig', [
         ]);
