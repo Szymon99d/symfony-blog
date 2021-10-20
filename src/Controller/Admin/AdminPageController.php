ca0e797a8cd4edf859b7ff31b02aa6156b895f6e
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-10-20 20:51:46 +0200

    Display website pages in table

--- src/Controller/Admin/AdminPageController.php
+++ src/Controller/Admin/AdminPageController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Page;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminPageController extends AbstractController{
     #[Route(['en'=>'/admin/page-panel','pl'=>'/admin/panel-stron'],name: 'app_admin_page_panel')]
-    public function pagePanel(): Response{
+    public function pagePanel(EntityManagerInterface $em): Response{
+        $pages = $em->getRepository(Page::class)->findAll();
         return $this->render('/admin/pages/page_panel.html.twig',[
-
+            'pages'=>$pages
+        ]);
+    }
+    #[Route(['en'=>'/admin/page-edit/{page}','pl'=>'/admin/edytuj-stronę/{page}'],name: 'app_admin_page_edit')]
+    public function pageEdit(EntityManagerInterface $em, Page $page): Response{
+        return $this->render('/admin/pages/page_panel.html.twig',[
         ]);
     }
    
